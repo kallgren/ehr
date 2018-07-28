aa94ddb5682a2e99c5f1f9939e2d8b5d0282a1cd
Robert Källgren <robertkallgren@gmail.com>
2018-07-28 22:48:20 +0200

    Simplified navigation bar by removing .left and .right container divs. This fixes an issue where there was a curious space below the ul tags causing te height of the navigation bar to be 53px instead of 50px. I had solved it previously by specifying an explicit height, but now I found a cleaner solution: use display flex on the parent of the ul tags. I'm not sure why it works, but it does. I did this by simply removing the .left and .right container divs which reduces the complexity, but the same result could probably be achieved by simply setting display flex on .left and .right as well.

--- src/components/TheNavigationBar.vue
+++ src/components/TheNavigationBar.vue
@@ -1,41 +1,37 @@
 <template>
   <nav id="nav">
-    <div class="left">
-      <ul>
-        <li><a href="#"><TheNavigationBarMenuButton/></a></li>
-      </ul>
-      <div class="divider"></div>
-      <ul>
-        <li><a href="#">Your inbox</a></li>
-        <li><a href="#">Notes to sign</a></li>
-        <li><a href="#">Your schedule</a></li>
-        <li><a href="#">Referrals to you</a></li>
-      </ul>
-      <div class="divider"></div>
-      <ul>
-        <li><router-link to="/">Dashboard</router-link></li>
-        <li><router-link to="/journal">Journal</router-link></li>
-        <li><a href="#">Medicine</a></li>
-        <li><a href="#">Test ordering</a></li>
-        <li><a href="#">Test results</a></li>
-        <li><a href="#">Referrals</a></li>
-        <li><a href="#">Sick leaves</a></li>
-      </ul>
-    </div>
-    <div class="right">
-      <ul>
-        <li>
-          <div class="user">
-            <span class="user-name">
-              {{ currentUser.first_name }} {{ currentUser.last_name }}
-            </span>
-            <br />
-            {{ currentUser.role }}
-          </div>
-          <a href="#">Log out</a>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li><a href="#"><TheNavigationBarMenuButton/></a></li>
+    </ul>
+    <div class="divider"></div>
+    <ul>
+      <li><a href="#">Your inbox</a></li>
+      <li><a href="#">Notes to sign</a></li>
+      <li><a href="#">Your schedule</a></li>
+      <li><a href="#">Referrals to you</a></li>
+    </ul>
+    <div class="divider"></div>
+    <ul>
+      <li><router-link to="/">Dashboard</router-link></li>
+      <li><router-link to="/journal">Journal</router-link></li>
+      <li><a href="#">Medicine</a></li>
+      <li><a href="#">Test ordering</a></li>
+      <li><a href="#">Test results</a></li>
+      <li><a href="#">Referrals</a></li>
+      <li><a href="#">Sick leaves</a></li>
+    </ul>
+    <ul class="user-section">
+      <li>
+        <div class="user-info">
+          <span class="user-name">
+            {{ currentUser.first_name }} {{ currentUser.last_name }}
+          </span>
+          <br />
+          {{ currentUser.role }}
+        </div>
+        <a href="#">Log out</a>
+      </li>
+    </ul>
   </nav>
 </template>
 
@@ -66,30 +62,24 @@ $navbar-height = 50px
 
 #nav
   display flex
-  height $navbar-height
   font-size $font-size--small
   color white
   background $color-green-dark
 
-.left
-.right
-  display inline-block
-
-.right
-  margin-left auto
-
 ul
-  display inline-block
   margin 0
   padding 0
   list-style-type none
+
+.user-section
+  margin-left auto
 
 li
   display table
   float left
 
   a
-  .user
+  .user-info
     display table-cell
     height $navbar-height
     vertical-align middle
@@ -103,7 +93,7 @@ li
     &.router-link-exact-active
       background $color-green-medium
 
-  .user
+  .user-info
     position relative
     padding 0 40px 0 60px
 
@@ -119,7 +109,6 @@ li
     font-size $font-size--medium
 
 .divider
-  display inline-block
   height 38px
   margin "calc((%s - 38px) / 2) 5px" % $navbar-height
   border-right 1px solid rgba(255, 255, 255, 0.8)
