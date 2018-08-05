0feb2880201c19c2fc41a58161e21abec2696a98
Robert Källgren <robertkallgren@gmail.com>
2018-08-05 18:00:32 +0200

    yarn/vue-cli-service lint auto-fix

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   css: {
     loaderOptions: {
       stylus: {
         import: [
-          path.resolve(__dirname, 'src/assets/styl/variables.styl'),
-          path.resolve(__dirname, 'src/assets/styl/mixins.styl')
+          path.resolve(__dirname, "src/assets/styl/variables.styl"),
+          path.resolve(__dirname, "src/assets/styl/mixins.styl")
         ]
       }
     }
   }
-}
+};
